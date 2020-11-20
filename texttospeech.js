c3f73799d7098c3e296d98cbcaf359a146939229
Ak500k <72781877+Ak500k@users.noreply.github.com>
2020-11-20 16:12:55 +0530

    Add files via upload

---
+++ texttospeech.js
@@ -0,0 +1,13 @@
+function textToAudio(){
+    let msg = document.getElementById("text-to-speech").value;
+
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
